29f95a9c4168faaf177864f82b017c9a3b5d06fd
Koray Baki <bakiikoray@gmail.com>
2026-09-02 17:46:17 +0300

    Switching to Vercel

--- script.js
+++ script.js
@@ -104,8 +104,8 @@ document.addEventListener("DOMContentLoaded", () => {
     }
 
     // Vercel Uyumlu API Endpoint'leri
-    const CONTACT_FUNCTION_URL = 'https://milat-ve-otesi.vercel.app/api/soruSor';
-    const AI_FUNCTION_URL = '/api/soruSor';
+    const CONTACT_FUNCTION_URL = 'https://milat-ve-otesi.vercel.app/api/sendMessage';
+    const AI_FUNCTION_URL = 'https://milat-ve-otesi.vercel.app/api/soruSor';
 
     const contactForm = document.getElementById('contact-form');
     const contactStatus = document.getElementById('contact-status');
